2149008f9ec90028cf38a049a1cb08da645b4dcc
ibrahimmhsaeed <ibrahimMhsaeed@gmail.com>
2023-11-17 13:03:53 -0800

    m

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,21 +10,29 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node = malloc(sizeof(list_t));
-	list_t *curr = head;
+	list_t *curr;
 
 	if (node == NULL)
 		return (NULL);
 
 	node->str = strdup(str);
 	node->len = strlen(str);
-
-	while (curr->next != NULL)
-	{
-		curr = curr->next;
-	}
-
-	curr->next = node;
 	node->next = NULL;
 
-	return (*node);
+	if (*head == NULL)
+	{
+		*head = node;
+	} else
+	{
+		curr = *head;
+
+		while (curr->next != NULL)
+		{
+			curr = curr->next;
+		}
+
+		curr->next = node;
+	}
+
+	return (node);
 }
