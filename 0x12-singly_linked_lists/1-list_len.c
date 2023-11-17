f3e6cfd007c1c0f7f85686f50a4ef836f7a4d603
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-17 22:02:05 +0200

    adding list len function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - count list element
+ * @h: pointer for list header
+ * Return: number of nodes in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *curr = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (curr != NULL)
+	{
+		curr = curr->next;
+		count++;
+	}
+
+
+	return (count);
+}
