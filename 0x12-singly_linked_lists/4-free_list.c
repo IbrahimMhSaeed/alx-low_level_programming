958734bcc375d233862ce589e9aab24c4c92e690
ibrahimmhsaeed <ibrahimMhsaeed@gmail.com>
2023-11-17 13:24:51 -0800

    m

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_list - count list element
+ * @head: pointer for list header
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+
+	list_t *next, *curr;
+
+	if (head == NULL)
+		return;
+
+	next = head->next;
+
+	free(head->str);
+	free(head->next);
+	free(head);
+
+	while (next != NULL)
+	{
+		curr = next;
+		next = curr->next;
+		free(curr->str);
+		free(curr->next);
+		free(curr);
+	}
+
+	return;
+
+}
