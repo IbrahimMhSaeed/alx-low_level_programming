5a8aa3f2cf3e3291c297cee921386b548527b236
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-18 13:09:55 +0200

    m

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * is_prime_number - prints
+ * @n: string to print
+ * Return: length;
+ */
+
+int check_prime(int n, int othrn);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+
+/**
+ * check_prime - prints
+ * @n: val
+ * @othrn: val
+ * Return: int
+ */
+
+int check_prime(int n, int othrn)
+{
+	if (othrn >= n && n > 1)
+		return (1);
+	else if (n % othrn == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, othrn + 1));
+
+}
