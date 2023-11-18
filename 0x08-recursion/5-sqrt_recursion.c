ede8887d690f711e9cdfbdae5d1e95d28b2c3d91
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-18 12:55:59 +0200

    m

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#insqclude "main.h"
+
+
+int square(int n, int val);
+
+/**
+ * _sqrt_recursion - prints
+ * @n: string to print
+ * Return: length;
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - prints
+ * @n: val
+ * @val: val
+ * Return: int
+ */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+
+}
