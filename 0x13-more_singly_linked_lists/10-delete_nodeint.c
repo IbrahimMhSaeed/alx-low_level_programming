429fc94f4e62805efef60feee5f862e3cca9385a
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-21 20:08:58 +0200

    m

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *del;
 	unsigned int count = 0;
 
+	if (*head == NULL)
+		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
