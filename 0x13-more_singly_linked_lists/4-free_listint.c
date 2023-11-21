aad487323b5a83f59a56c913f05d1354b47837b2
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-21 18:34:06 +0200

    m

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,35 +1,22 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - print list of numbers
+ * free_listint - print list of numbers
  * @head: pointer to head of list
- * @n: number integer
- * Return: size_t integer returned
+ * Return: Void
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+void free_listint(listint_t *head)
 {
-
-	listint_t *node = malloc(sizeof(listint_t));
-	listint_t *curr = *head;
-
-	if (!node)
-		return (NULL);
-
-	node->n = n;
-	node->next = NULL;
+	listint_t *curr;
 
 	if (!head)
-	{
-		*head = node;
-	} else
-	{
-		while (curr->next)
-		{
-			curr = curr->next;
-		}
-		curr->next = node;
-	}
+		return ;
 
-	return (*node);
+	while (head)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
 }
