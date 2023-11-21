92cc8de5e81d59747a8903f5a606c327d82de9fd
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-21 18:13:08 +0200

    m

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - print list of numbers
+ * @head: pointer to head of list
+ * @n: number integer
+ * Return: size_t integer returned
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+
+	listint_t *node = malloc(sizeof(listint_t));
+	listint_t *curr = *head;
+
+	node->n = n;
+	node->next = NULL;
+
+	if (!node)
+		return (NULL);
+
+	if (!head)
+	{
+		*head = node;
+	} else
+	{
+		while (curr->next)
+		{
+			curr = curr->next;
+		}
+		*curr->next = node;
+	}
+
+	return (*head);
+}
