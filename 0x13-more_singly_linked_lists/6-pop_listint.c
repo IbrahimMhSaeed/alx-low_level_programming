631f11bad7743bfa3c83ca8dab2105ddf6b13ced
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-21 19:10:18 +0200

    m

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - print list of numbers
+ * @head: pointer to head of list
+ * Return: vlaue of poped element
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	int n = temp->n;
+	*head = (*head)->next;
+
+	free(temp);
+	
+	return (n);
+}
