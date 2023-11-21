ef380b60209136fd03cf5d9596d94d2a970daa14
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-11-21 17:51:01 +0200

    m

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,20 @@
 #include "lists.h"
 
+/**
+ * print_listint - print list
+ * @h: pointer to head
+ * Return: size_t integer
+ */
 
 size_t print_listint(const listint_t *h)
 {
 
-	int count = 0;
+	size_t count = 0;
 
 	while (h)
 	{
 		count++;
-		printf("%i\n", count);
+		printf("%lu\n", count);
 		h = h->next;
 	}
 
