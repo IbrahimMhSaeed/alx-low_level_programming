1a551f7860ce577c619a2880a00187bd10a02fe2
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-03 19:45:53 +0200

    m

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,17 +4,11 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i;
-	if (index > 64)
-		return (-1);
-
-	i = (*n) >> index;
-
-	if ((i & 1) == 1)
-		return (-1);
-
-	*n += (2 << (index - 1));
-
-	return (1);
-
+	if (index < 64)
+	{
+		*n += 1 << index;
+		return (1);
+	}
+	
+	return (-1);
 }
