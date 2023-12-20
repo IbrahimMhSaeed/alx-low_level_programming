e70f01ef44a4615559cef83239faa77a1560e1a3
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-20 03:36:49 +0200

    m

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - get bit value
+ * @n: number to get bit
+ * @index: index of bit to get
+ * Return: always 1 (success)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > 64 || !n)
+		return (-1);
+
+	(*n &= 0 << index);
+	
+	return (1);
+}
