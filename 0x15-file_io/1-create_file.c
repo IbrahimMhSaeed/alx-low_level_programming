f965e2ba8621d5bc9015b92eb1df8a0b6ed051ed
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-23 14:26:54 +0200

    m

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - create file and write to it
+ * @filename: name of file
+ * @text_content: content to ber written in file
+ * Return: always 1 (success)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr;
+	ssize_t letters = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[letters] != NULL)
+		letters++;
+
+	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
+	wr = write(STDOUT_FILENO, text_content, letters);
+
+	if (fd == -1 || wr == -1 || wr != letters)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
