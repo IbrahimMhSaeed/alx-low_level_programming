350f654a1eb33f1020aa135e1ae5b69c14e33370
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-23 15:05:07 +0200

    m

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,10 +9,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, wr;
+	size_t letters;
+
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-		while (
+	letters = 0;
 
+	if (text_content != NULL)
+		while (text_content[letters])
+			letters++;
+
+	fd = open(filename, O_WRONLY|O_APPEND);
+	wr = write(fd, text_content, letters);
+
+	if (fd == -1 || wr == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 }
