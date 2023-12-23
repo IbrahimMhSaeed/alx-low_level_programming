017ad3ea652fa8a6df1c5db21a3e990ec6744221
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-23 14:11:24 +0200

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,25 +16,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd < 0)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
 		return (0);
 
+	fd = open(filename, O_RDONLY);
 	rd = read(fd, buffer, letters);
-
-	if (rd <= 0)
-		return (0);
-
 	wr = write(STDOUT_FILENO, buffer, rd);
 
-	if (wr < rd)
+	if (fd == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(buffer);
 		return (0);
+	}
 
+	free(buffer);
 	return (wr);
 }
