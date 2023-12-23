0827535a8f76530b438755a08cb681e91dd68b33
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-23 14:53:10 +0200

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,19 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rd, wr;
+	int fd, wr;
 	size_t letters = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[letters] != NULL)
-		letters++;
+	if (text_content != NULL)
+	{
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		while (text_content[letters])
+			letters++;
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	wr = write(fd, text_content, letters);
 
 	if (fd == -1 || wr == -1 )
