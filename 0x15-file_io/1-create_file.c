8e428f054688941d0decec26d6e009b26e12f84d
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-23 15:00:40 +0200

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wr;
-	size_t letters = 0;
+	size_t letters;
 
 	if (filename == NULL)
 		return (-1);
+
+	letters = 0;
 
 	if (text_content != NULL)
 		while (text_content[letters])
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text to the end of created file
+ * @filename: name of the file
+ * @text_content: text to be appended
+ * Return: always 1 (Success)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		while (
+
+}
