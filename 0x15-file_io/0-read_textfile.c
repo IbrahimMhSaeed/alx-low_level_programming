230855a858deaf7348f9c3d5cdbc7754e2d8a12a
IbrahimMhSaeed <ibrahimMhSaeed.com>
2023-12-23 14:03:04 +0200

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -31,9 +31,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rd <= 0)
 		return (0);
 
-	wr = write(STDOUT_FILENO, buffer, rd);
+	wr = write(STDOUT_FILENO, buffer, letters);
 
-	if (wr <= 0)
+	if (wr <= letters)
 		return (0);
 
 	return (wr);
